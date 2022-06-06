0628ca18fc9a2ecc3fd3b6b8b5eceb9b5bc0995d
!ApexNAM <46587415+ApexNAM@users.noreply.github.com>
2022-06-06 20:21:56 +0900

    daftGame 변수를 전역변수에서 지역변수로 변경.

--- Engine/Sources/main.cpp
+++ Engine/Sources/main.cpp
@@ -1,10 +1,8 @@
 #include "DaftGame.h"
 
-DaftGame* daftGame = nullptr;
-
 int main(int argc, char* argv[])
 {
-	daftGame = new DaftGame();
+	DaftGame* daftGame = new DaftGame();
 
 	daftGame->Init("Daft Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 	
