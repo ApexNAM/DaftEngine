a6cf6fe3368aa5ae5cd659993741ec0baa35bce3
!ApexNAM <46587415+ApexNAM@users.noreply.github.com>
2022-06-09 01:20:33 +0900

    glsl 파일 추가, 일부 코드 수정

--- Engine/Sources/Core/DaftGame.cpp
+++ Engine/Sources/Core/DaftGame.cpp
@@ -13,6 +13,12 @@ bool DaftGame::Initialize()
 {
 	glfwInit();
 
+	if (!glfwInit())
+	{
+		std::cout << "Error : Failed GLFW" << std::endl;
+		return false;
+	}
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
